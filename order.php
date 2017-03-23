64ab3a5d74da187cd0eeca01ffb332e30c7ce9ed
George Mponos <gmponos@gmail.com>
2017-03-23 21:22:04 +0200

    Added the deliveras iframe to the site

---
+++ order.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap/3.3.0/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href='https://fonts.googleapis.com/css?family=Roboto'>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <div id="deliverygr" class="delivery">
+            <iframe src="https://www.delivery.gr/%CE%91%CE%B8%CE%AE%CE%BD%CE%B1/%CE%91%CE%B9%CE%B3%CE%AC%CE%BB%CE%B5%CF%89/%CE%9C%CE%B1%CE%BC%CE%B1%CE%B3%CE%B5%CE%B9%CF%81%CE%B5%CE%BC%CE%B1%CF%84%CE%B1%20web%20site-9294/online"
+                    id="deliverygr">
+            </iframe>
+        </div>
+    </body>
+</html>
